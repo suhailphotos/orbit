2441277c5dd8e9f6e8a4314f71d768034d1e876d
Suhail <suhailece@gmail.com>
2025-08-05 16:54:32 -0700

    update 00-global.zsh

--- modules/env/00-global.zsh
+++ modules/env/00-global.zsh
@@ -3,5 +3,6 @@
 orbit_prepend_path "$HOME/.local/bin"   # pipx installs, etc.
 
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export DROPBOX="$HOME/Dropbox"
 export MATRIX="$DROPBOX/matrix"
 export BASE_DIR="$MATRIX/shellscripts"
