8a35666b5a4ea708088168dab5bfb9e3e8b579c3
Suhail Suhail <suhailece@gmail.com>
2025-08-29 11:11:17 -0700

    Add tmux functions

---
+++ modules/functions/tmux.zsh
@@ -0,0 +1,13 @@
+# modules/functions/tmux.zsh
+_starship_tmux_seg() {
+  if [[ -n "$TMUX" ]]; then
+    if [[ "$PWD" == "$HOME" ]]; then
+      export STARSHIP_TMUX_SEG='󱗾'       # at home: no surrounding spaces
+    else
+      export STARSHIP_TMUX_SEG=' 󱗾 '     # not home: space-prefixed & suffixed
+    fi
+  else
+    unset STARSHIP_TMUX_SEG               # hide when not in tmux
+  fi
+}
+precmd_functions+=(_starship_tmux_seg)
