1c9ea798a3ab8e03667fc76e2756e1e54d3137f0
Suhail Suhail <suhailece@gmail.com>
2025-09-06 14:04:26 -0700

    Add ripgrep env variables - syntax error fix

--- modules/env/16-search.zsh
+++ modules/env/16-search.zsh
@@ -1,19 +1,14 @@
 # modules/env/16-search.zsh
 # One ignore file to rule them all → rg, fd, Telescope
 
-# Respect XDG; fall back to ~/.config if not set
 : ${XDG_CONFIG_HOME:="$HOME/.config"}
 
-# Point ripgrep at your config (which points to the shared ignore)
+# ripgrep: point at your config file (which itself points at the shared ignore)
 _rgrc="$XDG_CONFIG_HOME/ripgrep/ripgreprc"
-if [[ -r "$_rgrc" ]]; then
-  export RIPGREP_CONFIG_PATH="$_rgrc"
-fi
+[[ -r "$_rgrc" ]] && export RIPGREP_CONFIG_PATH="$_rgrc"
 
-# fd doesn’t have a config file; use FD_OPTIONS
+# fd: use FD_OPTIONS (no special quoting needed; path has no spaces)
 _ignore="$XDG_CONFIG_HOME/search/ignore"
-if [[ -r "$_ignore" ]]; then
-  # Keep it minimal so you can still add flags ad hoc at the CLI
-  export FD_OPTIONS="--hidden --ignore-file=$(_ignore:q)"
-fi
+[[ -r "$_ignore" ]] && export FD_OPTIONS="--hidden --ignore-file=$_ignore"
+
 unset _rgrc _ignore
