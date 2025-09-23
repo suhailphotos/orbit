d4c7828e403b9d69fc0be856c6970fb3ed029e22
Suhail Suhail <suhailece@gmail.com>
2025-09-23 12:49:26 -0700

    Add Cargo target path

--- modules/env/12-cargo.zsh
+++ modules/env/12-cargo.zsh
@@ -2,3 +2,9 @@
 for d in "${CARGO_HOME:-$HOME/.cargo}/bin" "$HOME/.cargo/bin"; do
   [[ -d $d ]] && orbit_prepend_path "$d" && break
 done
+
+# Keep Cargo build artifacts out of Dropbox (and out of repos).
+# Can be overridden in a host file, e.g., modules/env/90-host-nimbus.zsh
+: ${CARGO_TARGET_DIR:="$HOME/.cache/cargo/targets"}
+export CARGO_TARGET_DIR
+mkdir -p "$CARGO_TARGET_DIR" 2>/dev/null || true
